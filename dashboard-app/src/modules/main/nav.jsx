4e521e696c0663f34ab78a72d60059776b936755
Eve_rest2 <yaelfavresectioninformatique@gmail.com>
2025-10-23 16:41:58 +0200

    Modifications faites sur la nouvelle version

--- dashboard-app/src/modules/main/nav.jsx
+++ dashboard-app/src/modules/main/nav.jsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
+
+function AnimationTable({ tab, tabs, activeTab, index, setActiveTab }) {
+  function handleClick(e) {
+    e.preventDefault();
+    setActiveTab(tabs[index])
+  }
+  return (<>
+    <div onClick={handleClick} className={(tab.id === activeTab.id) ? "AnimationTableActive" : "AnimationTableInactive"}> {tab.label} </div>
+  </>);
+}
+
+
 function DashboardMainNav() {
 
+  const [tabs, setTabs] = useState(
+    [
+      {
+        "id": 0,
+        "label": "Écran principal",
+      },
+      {
+        "id": 1,
+        "label": "Écran indices",
+      },
+      {
+        "id": 2,
+        "label": "Écran input",
+      },
+      {
+        "id": 3,
+        "label": "Dashboard",
+      }
+    ]
+  );
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
   return (
-    <nav>
-      [Navigation entre les tabs] 
-    </nav>
+    <section>
+      <div className="AnimationTable">
+        {tabs.map((tab, index) => {
+          return (<>
+            <AnimationTable tab={tab} index={index} activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs} />
+          </>)
+        })}
+
+      </div>
+    </section>
   )
+
 }
 
 export default DashboardMainNav
--- dashboard-app/src/modules/sidebar/storyline.jsx
+++ dashboard-app/src/modules/sidebar/storyline.jsx
@@ -2,7 +2,7 @@ import "./storyline.css";
 
 function DashboardSidebarStoryline() {
   return (<>
-    <div className="DashboardElementStoryframeCont">
+    <div className="StoryLine">
       <p style={{ display:"flex", flexFlow:"nowrap"}}> Trame de l'histoire </p>
     </div>
   </>)
